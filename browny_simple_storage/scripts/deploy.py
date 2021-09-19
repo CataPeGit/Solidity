5aa0d6248f66f5e5690f815762297ee49673e326
CataPeGit <74926354+CataPeGit@users.noreply.github.com>
2021-09-19 19:09:19 +0300

    Add files via upload

---
+++ browny_simple_storage/scripts/deploy.py
@@ -0,0 +1,16 @@
+from brownie import accounts, config, SimpleStorage
+#import os
+
+
+def deploy_simple_storage():
+    account = accounts[0]
+
+
+def main():
+    deploy_simple_storage()
+
+
+#account = accounts.load("MarcelBusiness-account")
+# print(account)
+#account = accounts.add(config["wallets"]["from_key"])
+# print(account)
